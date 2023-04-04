f0bba3505e6c126c27f2994d988c8eb217b3118f
Steve Aucapiña <saucapina@lorente.fin.ec>
2023-04-04 17:51:02 -0500

    addin insertion to bdd

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const smsController = require('../controllers/smsController');
 
+
 router.post('/sms/send', smsController.sendSms); 
 router.post('/sms/consulta-transaccion', (req, res) => {
     smsController.consultarEstadoTransaccion(req, res);
